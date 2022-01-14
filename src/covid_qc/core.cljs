5b4c8809d83679fdf4f3377bc86b1c1e5109fac5
Dan Fornika <dfornika@gmail.com>
2022-01-14 15:57:39 -0800

    Fix histogram bins

--- src/covid_qc/core.cljs
+++ src/covid_qc/core.cljs
@@ -301,7 +301,7 @@
                                                   {:type "number" :position "left"}]}}]]))
 
 (defn variants-histogram-plot []
-  (let [bins [[0 1] [1 2] [2 4] [4 6] [6 8] [8 12] [12 14] [14 16] [16 18] [18 20] [20 22] [22 24] [24 26] [26 28] [28 30] [30 32] [32 34] [34 36] [36 38] [38 40] [40 42]]
+  (let [bins [[0 1] [1 2] [2 4] [4 6] [6 8] [8 10] [10 12] [12 14] [14 16] [16 18] [18 20] [20 22] [22 24] [24 26] [26 28] [28 30] [30 32] [32 34] [34 36] [36 38] [38 40] [40 42]]
         select-data-keys #(select-keys % [:library_id :num_consensus_snvs :num_consensus_iupac :num_variants_indel])
         selected-plate-ncov-tools-qc-summary (get (:ncov-tools-qc-summaries @db) (:selected-plate-id @db))
         data (map select-data-keys selected-plate-ncov-tools-qc-summary)]
